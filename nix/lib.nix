4551006c2807ab361ea4db5e171afb4798da4fc2
oddlama <oddlama@oddlama.org>
2024-06-09 17:39:43 +0200

    chore: disable invalid shellcheck warning

--- nix/lib.nix
+++ nix/lib.nix
@@ -67,6 +67,7 @@
       # For master identies with no explicit pubkey, try extracting a pubkey from the file first.
       # Collect final identity arguments for encryption in an array.
       masterIdentityArgs=()
+      # shellcheck disable=SC2041
       for file in ${
         concatStringsSep " "
         (map
