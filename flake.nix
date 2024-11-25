cf28dcf49fd8ca7faed889206228104e9606e21e
oddlama <oddlama@oddlama.org>
2024-11-25 13:58:42 +0100

    feat: switch to flake-parts internally (no change for users)

--- flake.nix
+++ flake.nix
@@ -4,127 +4,128 @@
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  description = "An agenix extension adding secret generation and automatic rekeying using a YubiKey or master-identity";
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    devshell,
-    pre-commit-hooks,
-    ...
-  }: let
-    allApps = ["edit" "generate" "rekey"];
+  outputs = inputs: let
+    allApps = [
+      "edit"
+      "generate"
+      "rekey"
+    ];
   in
-    {
-      flakeModule = ./flake-module.nix;
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.flake-parts.flakeModules.flakeModules
+        inputs.pre-commit-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
 
-      nixosModules = {
-        agenix-rekey = import ./modules/agenix-rekey.nix nixpkgs;
-        agenixRekey = self.nixosModules.agenix-rekey; # backward compat
-        default = self.nixosModules.agenix-rekey;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      flake = {
+        config,
+        lib,
+        ...
+      }: {
+        flakeModule = ./flake-module.nix;
+        nixosModules = {
+          agenix-rekey = import ./modules/agenix-rekey.nix inputs.nixpkgs;
+          agenixRekey = config.nixosModules.agenix-rekey; # backward compat
+          default = config.nixosModules.agenix-rekey;
+        };
+        homeManagerModules = {
+          inherit (config.nixosModules) agenix-rekey;
+          default = config.homeManagerModules.agenix-rekey;
+        };
+
+        configure = {
+          # The path of the user's flake. Needed to run a sandbox-relaxed
+          # app that saves the rekeyed outputs.
+          userFlake,
+          # Configurations where agenix-rekey will search for attributes
+          nixosConfigurations ? {},
+          homeConfigurations ? {},
+          collectHomeManagerConfigurations ? true,
+          # Legacy alias for nixosConfigurations see https://github.com/oddlama/agenix-rekey/pull/51
+          nodes ? {},
+          # The package sets to use. pkgs.${system} must yield an initialized nixpkgs package set
+          pkgs ? config.pkgs,
+          # A function that returns the age package given a package set. Use
+          # this to override which tools is used for encrypting / decrypting.
+          # Defaults to rage (pkgs.rage). We only guarantee compatibility for
+          # pkgs.age and pkgs.rage.
+          agePackage ? (p: p.rage),
+          # The systems to generate apps for
+          systems ? [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ],
+        }:
+          lib.genAttrs systems (
+            system:
+              pkgs.${system}.lib.genAttrs allApps (
+                app:
+                  import ./apps/${app}.nix {
+                    nodes = import ./nix/select-nodes.nix {
+                      inherit
+                        nodes
+                        nixosConfigurations
+                        homeConfigurations
+                        collectHomeManagerConfigurations
+                        ;
+                      inherit (pkgs.${system}) lib;
+                    };
+                    inherit userFlake agePackage;
+                    pkgs = pkgs.${system};
+                  }
+              )
+          );
       };
 
-      homeManagerModules = {
-        inherit (self.nixosModules) agenix-rekey;
-        default = self.homeManagerModules.agenix-rekey;
-      };
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        devshells.default = {
+          packages = [
+            config.treefmt.build.wrapper
+          ];
+          devshell.startup.pre-commit.text = config.pre-commit.installationScript;
+        };
 
-      # A nixpkgs overlay that adds the agenix CLI wrapper
-      overlays.default = self.overlays.agenix-rekey;
-      overlays.agenix-rekey = _final: prev: {
-        agenix-rekey = prev.callPackage ./nix/package.nix {
+        pre-commit.settings.hooks.treefmt.enable = true;
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            deadnix.enable = true;
+            statix.enable = true;
+            nixfmt.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+
+        packages.default = pkgs.callPackage ./nix/package.nix {
           inherit allApps;
         };
+        overlayAttrs.agenix-rekey = config.packages.default;
       };
-
-      configure = {
-        # The path of the user's flake. Needed to run a sandbox-relaxed
-        # app that saves the rekeyed outputs.
-        userFlake,
-        # Configurations where agenix-rekey will search for attributes
-        nixosConfigurations ? {},
-        homeConfigurations ? {},
-        collectHomeManagerConfigurations ? true,
-        # Legacy alias for nixosConfigurations see https://github.com/oddlama/agenix-rekey/pull/51
-        nodes ? {},
-        # The package sets to use. pkgs.${system} must yield an initialized nixpkgs package set
-        pkgs ? self.pkgs,
-        # A function that returns the age package given a package set. Use
-        # this to override which tools is used for encrypting / decrypting.
-        # Defaults to rage (pkgs.rage). We only guarantee compatibility for
-        # pkgs.age and pkgs.rage.
-        agePackage ? (p: p.rage),
-      }:
-        (flake-utils.lib.eachDefaultSystem (system: {
-          apps = pkgs.${system}.lib.genAttrs allApps (app:
-            import ./apps/${app}.nix {
-              nodes = import ./nix/select-nodes.nix {
-                inherit nodes nixosConfigurations homeConfigurations collectHomeManagerConfigurations;
-                inherit (pkgs.${system}) lib;
-              };
-              inherit userFlake agePackage;
-              pkgs = pkgs.${system};
-            });
-        }))
-        .apps;
-    }
-    // flake-utils.lib.eachDefaultSystem (system: rec {
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          devshell.overlays.default
-          self.overlays.default
-        ];
-      };
-
-      # `nix build`
-      packages.default = packages.agenix-rekey;
-      # `nix build .#agenix-rekey`
-      packages.agenix-rekey = pkgs.agenix-rekey;
-
-      # `nix run`
-      apps.default = flake-utils.lib.mkApp {drv = packages.agenix-rekey;};
-
-      # `nix flake check`
-      checks.pre-commit-hooks = pre-commit-hooks.lib.${system}.run {
-        src = nixpkgs.lib.cleanSource ./.;
-        hooks = {
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-        };
-      };
-
-      # `nix develop`
-      devShells.default = pkgs.devshell.mkShell {
-        name = "agenix-rekey";
-        commands = with pkgs; [
-          {
-            package = alejandra;
-            help = "Format nix code";
-          }
-          {
-            package = statix;
-            help = "Lint nix code";
-          }
-          {
-            package = deadnix;
-            help = "Find unused expressions in nix code";
-          }
-        ];
-
-        devshell.startup.pre-commit.text = self.checks.${system}.pre-commit-hooks.shellHook;
-      };
-    });
+    };
 }
